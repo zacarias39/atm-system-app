a22465373560324ab5d568179e9907aecd39b7e6
zacarias39 <zacarias.natxo.39@gmail.com>
2025-01-03 23:55:09 +0100

    pin change completed

--- srcs/account_info.c
+++ srcs/account_info.c
@@ -12,9 +12,13 @@ void	account_info(t_user user)
 	green("\n=====-----===>"); yellow("  DON'T SHARE THIS!  "); green("<===-----=====");
 	yellow("\n=================================================");
 	green("\n=====->>"); blue("ACCOUNT NUMBER:\t"); printf("\033[1;31m%06d", user.account);
+	green("\n=====->>"); blue("BIC/SWIFT:\t"); red("SWNBFR22");
 	green("\n=====->>"); blue("ACCOUNT NAME:\t"); red(user.name);
 	green("\n=====->>"); blue("ACCOUNT PHONE:\t"); red(user.phone);
-	green("\n=====->>"); blue(" USERNAME:\t"); red(user.username);
+	green("\n=====->>"); blue("USERNAME:\t"); red(user.username);
+	green("\n=====->>"); blue("COUNTRY:\t"); red("ANGOLA");
+	green("\n=====->>"); blue("AGENCY:\t\t"); red("00100");
+	green("\n=====->>"); blue("ADDRESS:\t"); red("AVENIDA ZCASIMIR");
 	green("\n=====-----====>"); yellow(" KEEP YOUR DETAILS "); green("<====-----=====");
 	green("\n=====-----====>"); yellow("       SAFE        "); green("<====-----=====");
 	green("\n=====--====>"); yellow("   THANK YOU FOR USING   "); green("<====--=====");
